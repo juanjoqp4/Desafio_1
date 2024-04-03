f1e3a2c0ae9567cc276bdb6a335bd547c0f95196
juanjoqp4 <juan.quicenop@udea.edu.co>
2024-04-03 16:53:39 -0500

    Commit #21 / Dia 10 / Actualizacion main

--- Desafio_1/Func_1.h
+++ Desafio_1/Func_1.h
@@ -5,19 +5,19 @@
 
 #include <iostream>     //Entrada y salida estandar (cout y cin)
 #include <stdlib.h>      //Manejo de memoria dinamica (new y delete)
-#include <string.h>
+#include <string.h>     //Sirve para la eleccion de repetir o no xD
 
 using namespace std;
 
-int ***mat;
-int *pref, *k;
+int ***mat;                 //Estructura de datos M
+int *pref, *k;              //pref guarda el orden de cada matriz en M //k es la encargada de guarda la entrada K
 
 
 //Funcion para imprimir la matriz ( Meramente grafica )
 void Graph(int matriz){
-    for(int i = 0; i < pref[matriz]; i++){
+    for(int i = 0; i < pref[matriz]; i++){              //Itera en funcion a el orden de la matriz dada en el argumento
         for(int j = 0; j < pref[matriz]; j++){
-            if(mat[matriz][i][j] != 0){
+            if(mat[matriz][i][j] != 0){                 //Si se encuentra en el punto medio no imprime
                 if(mat[matriz][i][j] < 10){
                     cout << mat[matriz][i][j] << "  ";              //Para compensar espacios, en caso de que sea de 1 digito añade un espacio mas
                 }else cout << mat[matriz][i][j] << " ";
@@ -38,51 +38,51 @@ int User_Bob(){
     k = new int [5];
 
     cout << "Digite el orden de cada una" << endl;
-    for(int i = 0; i < est; i++){
+    for(int i = 0; i < est; i++){                               //Este ciclo se encarga de darle a cada matriz un orden
         cout << "Orden para estructura #" << i + 1 << " : ";
         cin >> orden;
         while ((orden < 3) || (orden % 2 == 0)){                                                            //Validacion
             cout << "Orden invalido, vuelva a intentarlo" << endl << "Estructura #" << i + 1 << " : ";
             cin >> orden;
         }
-        pref[i] = orden;
+        pref[i] = orden;            //En la posicion i se guarda n orden, se podria decir que pref esta alineada a mat (primer nivel)
     }
 
     mat = new int **[est];              //CERRADURA X
-    for (int i = 0; i < est; i++){      //i referencia cada estructura
-        mat[i] = new int *[pref[i]];           //Aqui se le asigna numero de filas a la matriz del tamaño de estructura i
+    for (int i = 0; i < est; i++){      //i referencia cada estructura  (1)
+        mat[i] = new int *[pref[i]];           //Aqui se le asigna numero de filas a la matriz del tamaño de estructura i (2)
         for(int j = 0; j < pref[i]; j++){
-            mat[i][j] = new int [pref[i]];      //Se le asigna numero de columnas a cada fila del tamaño de estructura i
+            mat[i][j] = new int [pref[i]];      //Se le asigna numero de columnas a cada fila del tamaño de estructura i (3)
         }
     }
 
-    return est;
+    return est;         //Retorna el numero de matrices que hay en el arreglo
 }
 //Fin
 
 //Funcion para liberar memoria dinamica
 void Eraser(int num_matrices){
     for(int i = 0; i < num_matrices; i++){
-        for(int j = 0; j < pref[i]; j++){
-            delete[] mat[i][j];             //Libera cada columna por fila
+        for(int j = 0; j < pref[i]; j++){       //Itera segun el orden de la matriz referenciada por i
+            delete[] mat[i][j];             //Libera cada columna por fila (nivel 3)
         }
-        delete[] mat[i];            //Libera cada fila
+        delete[] mat[i];            //Libera cada fila (nivel 2)
     }
     delete[] mat;           //Libera cada espacio para matrices
     mat = nullptr;
 
-    delete[] k;
+    delete[] k;         //Libera el espacio reservado para k
     k = nullptr;
 
-    delete[] pref;
+    delete[] pref;      //Libera el espacio reservado para pref
     pref = nullptr;
 }
 //Fin
 
 //Funcion que añade valores a cada una de las matrices
 void Value(int matriz){
-    int cont = 1, lineal = pref[matriz] / 2;
-
+    int cont = 1, lineal = pref[matriz] / 2;        //Tomo la mitad del orden en matriz, al ser impar siempre me va a dar
+                                                    //el punto medio de esa matriz, ejm: en una 7x7, va de 0 a 6 y su mitad es [3][3]
     for(int i = 0; i < pref[matriz]; i++){
         for(int j = 0; j < pref[matriz]; j++){
             if((i == lineal) && (j == lineal)){         //Si se encuentra en la mitad de la matriz asigna valor 0 a esa posicion
@@ -91,8 +91,20 @@ void Value(int matriz){
                 mat[matriz][i][j] = cont;
                 cont++;
             }
-        }
+        }                                           //Gracias al doble ciclo cada espacio por fila es ocupado por un numero consecutivo
     }
+}
+//Fin
+
+//Funcion encargada de cambiar el tamaño de una matriz
+void Dimension(int matriz){
+    mat[matriz] = new int *[pref[matriz] += 2];           //Aqui se le asigna numero de filas a la matriz del tamaño de estructura #matriz
+    for(int j = 0; j < pref[matriz]; j++){
+        mat[matriz][j] = new int [pref[matriz]];      //Se le asigna numero de columnas a cada fila del nuevo tamaño de estructura i
+    }
+    Value(matriz);                              //Se le agregan los valores a la nueva matriz
+    //cout << " / " << pref[matriz] << endl;
+    //Graph(matriz);                            //Pruebas
 }
 //Fin
 
@@ -108,54 +120,67 @@ void Rotation(int matriz){
 
             help = j - min;             //help ayuda a referenciar los nuevos limites, ya que no puedo modificar j
 
-            backup = mat[matriz][min][j];
+            backup = mat[matriz][min][j];       //Siempre habra un valor que se debe guardar, debido a que esa posicion sera ocupada por otro
 
-            mat[matriz][min][j] = mat[matriz][max - help][min];
+            mat[matriz][min][j] = mat[matriz][max - help][min];             //En cuanto a la rotacion siempre hay un patron con las posiciones, no los valores
 
-            mat[matriz][max - help][min] = mat[matriz][max][max - help];
+            mat[matriz][max - help][min] = mat[matriz][max][max - help];    //Siempre que un valor pasa a otra posicion, la posicion anterior debe ser ocupada por otro
 
             mat[matriz][max][max - help] = mat[matriz][j][max];
 
-            mat[matriz][j][max] = backup;
+            mat[matriz][j][max] = backup;       //En este caso el ultimo valor pasa a ser el primero que guardamos
         }
     }
-    //cout << "Rotacion : " << "Matriz #" << matriz + 1 << endl;
+    //cout << "Rotacion : " << "Matriz #" << matriz + 1 << endl;     //Pruebas
 }
 //Fin
 
 //Funcion encargada de validaciones de rango
 int Comp (int var, int range){
     while ((var > range) || (var < 1)){
-        cout << "Error, no corresponde con los limites\n" << "Digite nuevamente: ";
+        cout << "Error, no corresponde con los limites\n" << "Digite nuevamente: ";     //Me sirve cuando solo se puede escoger entre 1 y n
         cin >> var;
     }
-    return var;
+    return var;     //Retorna el valor que cumple con las condiciones
 }
 //Fin
 
 //Funcion encargada de construir la regla K
 void Rule_K(int num_matrices){
 
-    int fila, columna, est, m1, m2, orden;
+    int fila = 0, columna = 0, est = 0, m1 = 0, m2 = 0, orden = 0;          //No me gusta tenerlos divagando con valores sin sentido
 
     cout << "Regla K" << endl << "Digite # de estructura: ";
-    cin >> est;
-    est = Comp(est, num_matrices);
-    k[2] = est;
+    cin >> est;                                                 //Al comprobar primero la estructura deseada, puedo determinar facilmente su orden
+    est = Comp(est, num_matrices);                              // y realizar las respectivas validaciones
+    k[2] = est;                         //Se añade al arreglo
 
     orden = pref[est - 1];
 
     cout << "Digite Fila: ";
     cin >> fila;
-    fila = Comp(fila, orden);
+    fila = Comp(fila, orden);               //Se comprueba que corresponda al orden de esa matriz
     k[0] = fila;
 
     cout << "Digite Columna: ";
     cin >> columna;
-    columna = Comp(columna, orden);
+    columna = Comp(columna, orden);         //Se comprueba que corresponda al orden de esa matriz
     k[1] = columna;
 
-    cout << "Digite -1 para mayor / menor o 1 para menor / mayor: ";
+    while(mat[est - 1][fila - 1][columna - 1] == 0){
+        cout << "Esta coordenada corresponde al centro de la matriz, digite otra diferente" << endl;
+        cout << "Digite Fila: ";
+        cin >> fila;
+        fila = Comp(fila, orden);               //Se comprueba que corresponda al orden de esa matriz
+        k[0] = fila;
+
+        cout << "Digite Columna: ";
+        cin >> columna;
+        columna = Comp(columna, orden);         //Se comprueba que corresponda al orden de esa matriz
+        k[1] = columna;
+    }
+
+    cout << "Digite -1 o 1 para la secuencia: ";
     cin >> m1;
     while ((m1 != 1) && (m1 != -1)){
         cout << "Error\nDigite -1 para mayor o 1 para menor: ";
@@ -163,48 +188,70 @@ void Rule_K(int num_matrices){
     }
     k[3] = m1;
 
-    if (m1 == 1) m2 = -1;
-    else m2 = 1;
+    cout << "Digite -1 o 1 para la secuencia: ";
+    cin >> m2;
+    while ((m2 != 1) && (m2 != -1)){
+        cout << "Error\nDigite -1 para mayor o 1 para menor: ";
+        cin >> m2;
+    }
     k[4] = m2;
 }
 //Fin
 
 //Funcion para validar la apertura de X
 void Locked(int num_matrices){
-    bool comp = false, invalid = false;
-    int backup = 0, backup_2 = 0, fila = k[0] - 1, column = k[1] - 1, sec = 0, cont = 0, Escen = 1, DIF = 0, DIF_sup = 0;
+
+    bool comp = false;
+    int PIN, backup = 0, backup_2 = 0, fila = k[0] - 1, column = k[1] - 1, sec = 0, cont = 0, Escen = 0, DIF = 0, DIF_sup = 0;
+
     if (k[3] == -1) sec = -1;           //sec indica si la secuencia es mayor / menor(-1 / 1 /true) o menor / mayor(1 / -1 / false)
     else sec = 1;
 
-    for (int i = 0; i < num_matrices - 1; i++){
+
+    PIN = pref[k[2] - 1];               //Se guarda el orden de la primera matriz, asi no se pierde la proporcion entre matrices aunque la de referencia cambie su tamaño
+    for (int i = k[2] - 1; i < num_matrices - 1; i++){              //Inicia desde la estructura deseada
         comp = false;
         while (comp == false){
 
-            DIF = (pref[i] - pref[k[2] - 1]) / 2;
-            DIF_sup = (pref[i + 1] - pref[k[2] - 1]) / 2;      //DIF indica la diferencia entre ordenes de la matriz i / i+1 y la estructura elegida, para que al referenciar posiciones sea proporcional al centro de cada una
+            while((fila > pref[i + 1]) || (column > pref[i + 1])){
+                cout << endl << "Las dimensiones podrian no corresponder con la matriz " << i + 2 <<",\npor lo que su orden aumentara" << endl;
+                Dimension(i + 1);
+            }                           //Se aplica por si las coordenadas digitadas superan el orden de algunas matrices
 
-            if (DIF < 0) DIF *= -1;
+            DIF = (pref[i] - PIN) / 2;                                  //DIF y DIF_sup indican la diferencia entre ordenes de la matriz i / i + 1 y
+            DIF_sup = (pref[i + 1] - PIN) / 2;                     //la estructura elegida (PIN), para que al indicar posiciones sean proporcionales al centro de cada una
 
-            if ((cont != 0) && (pref[i] != pref[k[2] - 1])) backup = mat[i][fila + DIF][column + DIF];
-            else backup = mat[i][fila][column];
+            if ((((cont != 0) || (pref[i] != PIN)) && (pref[i] != PIN))){   //Puede haber un escenario en que la matriz i haya cambiado de tamaño, por lo que se debe tomar como una de distinto tamaño
 
-            if(pref[k[2] - 1] != pref[i + 1]) backup_2 = mat[i + 1][fila + DIF_sup][column + DIF_sup];
-            else backup_2 = mat[i + 1][fila][column];
+                if((fila + DIF >= 0) && (column + DIF >= 0)) backup = mat[i][fila + DIF][column + DIF];
+                else if((fila + DIF < 0) && (column + DIF >= 0)) backup = mat[i][fila + DIF][column];                        //En caso de que alguna de las coordenadas menos la diferencia sean menores a 0,
+                else if ((column + DIF < 0) && (fila + DIF >= 0)) backup = mat[i][fila][column + DIF];                    //no se aplicara la diferencia
+                else backup = mat[i][fila][column];
 
-            if(Escen < 4){
-                if (sec == -1){
+            }else backup = mat[i][fila][column];         //En el caso de que sea del mismo orden a PIN, no hay necesidad de aplicar proporciones
+
+            if(PIN != pref[i + 1]){     //Si son de diferente orden se aplica la diferencia
+
+                if((fila + DIF_sup >= 0) && (column + DIF_sup >= 0)) backup_2 = mat[i + 1][fila + DIF_sup][column + DIF_sup];
+                else if((fila + DIF_sup < 0) && (column + DIF_sup >= 0)) backup_2 = mat[i + 1][fila + DIF_sup][column];                        //En caso de que alguna de las coordenadas menos la diferencia sean menores a 0,
+                else if ((column + DIF_sup < 0) && (fila + DIF_sup >= 0)) backup_2 = mat[i + 1][fila][column + DIF_sup];                    //no se aplicara la diferencia
+                else backup_2 = mat[i + 1][fila][column];
+
+            }else backup_2 = mat[i + 1][fila][column];
+
+            if(Escen < 4){                  //Indica si la matriz dio todas las rotaciones necesarias
+                if (sec == -1){                 //Si la secuencia indica mayor / menor aplica el siguiente condicional
 
                     if(backup > backup_2){
                         comp = true;
                     }else Rotation(i + 1);
 
-                } else {
+                } else {                        //Si la secuencia indica menor / mayor aplica el siguiente condicional
                     if(backup < backup_2){
                         comp = true;
                     }else Rotation(i + 1);
                 }
-            }else{
-                invalid = true;
+            }else{                      //Si ya dio todas las vueltas necesarias, ahora la que gira es la matriz i
                 if (sec == -1){
 
                     if(backup > backup_2){
@@ -218,17 +265,20 @@ void Locked(int num_matrices){
                 }
             }
 
-            if ((Escen > 5) && (invalid == true)){
-                comp = true;
-                cout << endl <<"No hay forma de cumplir con las condiciones" << endl;
+            if ((Escen > 7) && (sec == - 1)){   //Si la secuencia indica mayor / menor, el orden de la matriz i cambia para poder cumplir con esa condicion
+                Dimension(i);
+                Escen = 0;
+            }else if ((Escen > 7) && (sec == 1)){   //Si la secuencia indica menor / mayor, el orden de la matriz i cambia para poder cumplir con esa condicion
+                Dimension(i + 1);
+                Escen = 0;          //Escen toma valor 0, para poder comprobar la matriz "nueva"
             }
-            Escen++;
+
+            Escen++;            //Contabiliza el numero de rotaciones
         }
-        if (sec == -1) sec += 2;
-        else sec -= 2;
-        cont++;
-        Escen = 1;
+        if (cont % 2 == 0) sec = k[4];              //Si cont es 0 por ejm (primera iteracion), toma el valor en la siguiente posicion de K
+        else sec = k[3];                            //Cuando sea impar toma la anterior
+        cont++;                 //Sirve para indicar la primera vuelta
+        Escen = 0;          //Toma valor 0 para contabilizar las vueltas de la nueva comparacion
     }
 }
 //Fin
-
--- Desafio_1/main.cpp
+++ Desafio_1/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     string rep;
-    do
+    do                  //Su finalidad es que se repita o no el programa una vez finalice
     {
 
     int num_matrices = User_Bob();          //Inicializa los arreglos a gusto del usuario
@@ -13,7 +13,7 @@ int main()
     Rule_K(num_matrices);       //Construye la regla K
 
     cout << endl << "Matrices construidas: " << endl;
-    for(int j = 0; j < num_matrices; j++) Graph(j);   //Imprime matrices segun le indique
+    for(int j = 0; j < num_matrices; j++) Graph(j);   //Imprime matrices segun le indique, simplemente para mostrar las matrices generadas
     cout << endl;
 
     cout << "Regla K : ";
@@ -22,40 +22,18 @@ int main()
         else cout << k[h] << endl;
     }
 
-    Locked(num_matrices);
+    Locked(num_matrices);       //Realiza las validaciones necesarias para la configuracion de apertura
 
-    cout << "Configuracion de apertura : " << endl << endl;
-    for(int j = 0; j < num_matrices; j++) Graph(j);
+    cout << endl << "Configuracion de apertura : " << endl << endl;
+    for(int j = 0; j < num_matrices; j++) Graph(j);                 //Imprime como quedo cada matriz despues de los cambios
 
     Eraser(num_matrices);       //Libera memoria reservada
 
     cout << "Digite 0 para salir o cualquier otra tecla para repetir: ";
     cin.ignore();
-    getline(cin , rep);
+    getline(cin , rep);                         //Con 0 repite el programa
     }while(rep != "0");
 
     cout << endl <<" --> Programa Finalizado" << endl;
     return 0;
 }
-
-
-//PROBLEMA : Al pensar que la secuencia la podia determinar ell usuario, surgia un problema en cuanto a las rotaciones, ya que al buscar
-        //un numero mas grande que el de mayor valor en la estructura entraba en un bucle infinito, por lo que la secuencia debia ser intercalada
-
-/*          Al no comprender bien el enunciado, pense que podria haber un escenario en el que la matriz cambiaba su tamaño
-//Funcion encargada de cambiar el tamaño de una matriz
-void Dimension(int matriz){
-    mat[matriz] = new int *[pref[matriz] += 2];           //Aqui se le asigna numero de filas a la matriz del tamaño de estructura #matriz
-    for(int j = 0; j < pref[matriz]; j++){
-        mat[matriz][j] = new int [pref[matriz]];      //Se le asigna numero de columnas a cada fila del tamaño de estructura i
-    }
-    Value(matriz);
-}
-//Fin
-*/
-
-//PROBLEMA : Al pensar que la secuencia la podia determinar el usuario, surgia un problema en cuanto a las rotaciones, ya que al buscar
-        //un numero mas grande que el de mayor valor en la estructura entraba en un bucle infinito, por lo que la secuencia debia ser intercalada
-
-//PROBLEMA : Revisar la operacion en [3][2]
-
